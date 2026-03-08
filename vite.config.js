6ebef1556ce848b9a8fb94f760b17f6f65940950
Thirith Heng <thirithheng@gmail.com>
2026-03-08 14:15:56 +0700

    Init

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: { host: true, port: 5174 },
+  optimizeDeps: {
+    include: [
+      'firebase/app',
+      'firebase/auth',
+      'firebase/firestore',
+      'firebase/database',
+      'firebase/storage',
+    ],
+  },
 })
